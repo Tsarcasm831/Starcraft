48f5cea1f87367d6efbf3004ced1ce02d4283730
Tsarcasm831 <88747937+Tsarcasm831@users.noreply.github.com>
2025-06-23 13:04:59 -0600

    Load SCV2 animations from manifest

--- src/game/preloader.js
+++ src/game/preloader.js
@@ -11,15 +11,6 @@ export async function preloadAssets(audioManager) {
     tasks.push(() => assetManager.loadGLB('assets/models/scv.glb', 'scv'));
     tasks.push(() => assetManager.loadGLB('https://file.garden/Zy7B0LkdIVpGyzA1/StarCraft/sounds/Terran/Units/SCV/scv2.glb', 'scv2'));
 
-    // Animations for SCV Mark 2
-    const scv2AnimationPaths = {
-        mineRepair: 'assets/models/animations/SCV/Animation_MineRepair.glb',
-        idle: 'https://file.garden/Zy7B0LkdIVpGyzA1/StarCraft/sounds/Terran/Units/SCV/Animation_Idle.glb',
-        walking: 'https://file.garden/Zy7B0LkdIVpGyzA1/StarCraft/sounds/Terran/Units/SCV/Animation_Walking.glb'
-    };
-    Object.entries(scv2AnimationPaths).forEach(([key, path]) => {
-        tasks.push(() => assetManager.loadGLB(path, `scv2_${key}`));
-    });
     tasks.push(() => assetManager.loadGLB('assets/models/vulture.glb', 'vulture'));
     tasks.push(() => assetManager.loadGLB('assets/models/goliath.glb', 'goliath'));
     tasks.push(() => assetManager.loadGLB('assets/models/wraith.glb', 'wraith'));
